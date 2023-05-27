1e0d9cf1fd88dfef8c058f4679ca1db0acb49a6f
DQue <xxdqxx@gmail.com>
2023-05-28 01:34:40 +0900

    JS/CSSファイル分離

---
+++ js.js
@@ -0,0 +1,85 @@
+
+function $(a) { return document.getElementById(a) }
+function ce(a) { return document.createElement(a) }
+function ct(a) { return document.createTextNode(a) }
+function cl(e) { while (e.firstChild) { e.removeChild(e.firstChild) } }
+
+window.addEventListener("DOMContentLoaded", () => {
+    更新();
+
+    $("i_btn").addEventListener("click", プレビュー追加, false);
+    $("i_btn_preset").addEventListener("click", プリセットプレビュー追加, false);
+    const input = document.getElementsByTagName("input");
+    for (let i = 0; i < input.length; i++) {
+        input[i].addEventListener("change", 更新, false);
+    }
+}, false)
+
+function 要素生成(bg, size, ttl) {
+    const e = ce("div");
+    e.className = "pv_child";
+
+    const title = e.appendChild(ce("h1"));
+    title.className = "pv_title";
+    if (ttl === undefined) ttl = `${size}px ${bg}`;
+    title.appendChild(ct(ttl))
+
+    const container = e.appendChild(ce("span"));
+    container.className = "pv_container";
+    container.style.backgroundColor = bg;
+
+    const img = container.appendChild(ce("img"));
+    img.className = "pv_img";
+    img.style.height = size + "px";
+    const url = URL.createObjectURL($("u_file").files[0])
+    img.src = url;
+
+    return e;
+}
+
+function 更新() {
+    const ec = $("o_color");
+    const es = $("o_size");
+
+    const image = $("u_file").files ? $("u_file").files.length : 0;
+    $("setting").hidden = image === 0 ? true : false;
+
+
+
+    cl(ec);
+    cl(es);
+
+    ec.appendChild(ct($("i_color").value));
+    es.appendChild(ct($("i_size").value));
+
+}
+
+function プレビュー追加() {
+    const color = $("i_color").value;
+    const size = $("i_size").value;
+    const e = 要素生成(color, size);
+    $("pv_parent").appendChild(e);
+}
+
+function プリセットプレビュー追加() {
+    const list = [
+        { title: "Mi Light 本文", bg: "rgb(255,255,255)", size: 29.3906 },
+        { title: "Mi Light リアクション欄", bg: "rgb(242,242,242)", size: 18.375 },
+        { title: "Mi Light リアクション欄(自分が押した)", bg: "rgb(134,179,0)", size: 18.375 },
+        { title: "Mi Light リアクション欄(でかい)", bg: "rgb(242,242,242)", size: 27.5625 },
+        { title: "Mi Light リアクション欄(自分が押した)(でかい)", bg: "rgb(134,179,0)", size: 27.5625 },
+        { title: "Mi Dark 本文", bg: "rgb(45,45,45)", size: 29.3906 },
+        { title: "Mi Dark リアクション欄", bg: "rgb(56,56,56)", size: 18.375 },
+        { title: "Mi Dark リアクション欄(自分が押した)", bg: "rgb(134,179,0)", size: 18.375 },
+        { title: "Mi Dark リアクション欄(でかい)", bg: "rgb(56,56,56)", size: 27.5625 },
+        { title: "Mi Dark リアクション欄(自分が押した)(でかい)", bg: "rgb(134,179,0)", size: 27.5625 },
+    ];
+
+    for (let i of list) {
+        const bg = i.bg;
+        const size = i.size;
+        const title = i.title;
+        const e = 要素生成(bg, size, title);
+        $("pv_parent").appendChild(e);
+    }
+}
